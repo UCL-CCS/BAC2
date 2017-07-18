dc37ede4b8994427eca0dd8885b0c200163fc568
kristofarkas <kristof.farkas-pall.14@ucl.ac.uk>
2017-07-18 10:29:15 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bac',
+
+    version='0.0.1.dev1',
+
+    description='Binding affinity calculator',
+
+    long_description='Copy from README file',
+
+    url='http://ccs.chem.ucl.ac.uk',
+
+    author='CCS',
+
+    packages=find_packages(),
+)
+
