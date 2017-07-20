105936c54cffd3a5c932889e0dcb919f197139b8
kristofarkas <kristof.farkas-pall.14@ucl.ac.uk>
2017-07-20 14:48:01 +0100

    Update gromacs classes

--- bac/simulate/gromacs/__init__.py
+++ bac/simulate/gromacs/__init__.py
@@ -1,1 +1,6 @@
-# FIXME: gromacs grps
+from .run_controller import Run
+
+from .pressure_controller import PressureController
+from .temperature_controller import TemperatureController
+from .non_bonded_controller import (NonBondedController)
+from .constraint_controller import (ConstraintController, ConstraintAlgorithmType, ConstraintType)
--- bac/simulate/gromacs/temperature_controller.py
+++ bac/simulate/gromacs/temperature_controller.py
@@ -4,7 +4,7 @@ from bac.utils.decorators import advanced_property, integer, decimal
 from bac.simulate.gromacs.run_controller import Integrator
 
 
-class TemperatureCouple(Enum):
+class TemperatureCouplingType(Enum):
     no = 'no'
     berendsen = 'berendsen'
     nose_hoover = 'nose-hoover'
@@ -14,34 +14,43 @@ class TemperatureCouple(Enum):
 
 
 class TemperatureController:
-
     def __init__(self, **kwargs):
 
-        self.coupling_type = kwargs.get('coupling_type')
-        self.coupling_frequency = kwargs.get('coupling_frequency')
+        self.type = kwargs.get('type')
+        self.frequency = kwargs.get('frequency')
         self.nose_hoover_chain_length = kwargs.get('nose_hoover_chain_length')
-        self.coupling_time = kwargs.get('coupling_time')
-        self.coupling_temperature = kwargs.get('coupling_temperature')
+        self.time = kwargs.get('time')
+        self.temperature = kwargs.get('temperature')
 
-    @advanced_property(type=TemperatureCouple, default=TemperatureCouple.no)
-    def coupling_type(self): pass
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return (self.type == other.type and
+                    self.frequency == other.frequency and
+                    self.nose_hoover_chain_length == other.nose_hoover_chain_length and
+                    self.time == other.time and
+                    self.temperature == other.temperature)
 
-    def coupling_frequency_default(self):
+        return False
+
+    @advanced_property(type=TemperatureCouplingType, default=TemperatureCouplingType.no)
+    def type(self): pass
+
+    def frequency_default(self):
         if self.run.integrator in (Integrator.md_vv,Integrator.md_vv_avek):
             return 1
         else:
             return self.run.non_bonded_controller.nstlist if self.run.non_bonded_controller.nstlist > 0 else 10
 
-    @integer(default=coupling_frequency_default)
-    def coupling_frequency(self): pass
+    @integer(default=frequency_default)
+    def frequency(self): pass
 
     @integer(default=10, validator=lambda v, o: (v == 1) if (o.run.integrator is Integrator.md) else True)
     def nose_hoover_chain_length(self): pass
 
     @decimal
-    def coupling_time(self): pass
+    def time(self): pass
 
     @decimal
-    def coupling_temperature(self): pass
+    def temperature(self): pass
 
 
