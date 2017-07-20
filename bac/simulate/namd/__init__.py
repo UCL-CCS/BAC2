ee57268e923481f34d1874c1694873635edd9b21
kristofarkas <kristof.farkas-pall.14@ucl.ac.uk>
2017-07-20 14:49:52 +0100

    Add some import to __init__ file

--- bac/simulate/namd/__init__.py
+++ bac/simulate/namd/__init__.py
@@ -1,1 +1,3 @@
 from .run_controller import Run
+from .temperature_controller import (TemperatureController, LangevinDynamics, TemperatureCoupling,
+                                     VelocityReassignment, VelocityRescaling, LoweAndersenDynamics)
