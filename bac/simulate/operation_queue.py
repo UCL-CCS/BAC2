ef774ccffd2d985f854a30d53a785802db6c3cc7
kristofarkas <kristof.farkas-pall.14@ucl.ac.uk>
2017-07-21 17:39:31 +0100

    Update operation_queue

--- bac/simulate/operation_queue.py
+++ bac/simulate/operation_queue.py
@@ -1,3 +1,5 @@
+from .operation import Operation
+
 class OperationQueue:
 
     def __init__(self, resource):
@@ -6,6 +8,10 @@ class OperationQueue:
 
     def add_operation(self, operation):
         self.operations.append(operation)
+
+    def add_run(self, run):
+        op = Operation(run=run)
+        self.add_operation(op)
 
     def execute(self):
         print('Executing on {}'.format(self.resource))
