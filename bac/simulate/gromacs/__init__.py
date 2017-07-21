f0fa6ff5d3f0d7d7c2f95abe9ab0385227f45e99
kristofarkas <kristof.farkas-pall.14@ucl.ac.uk>
2017-07-21 15:38:02 +0100

    Add back_reference to controllers

--- bac/simulate/gromacs/__init__.py
+++ bac/simulate/gromacs/__init__.py
@@ -1,6 +1,12 @@
-from .run_controller import Run
+from .run_controller import (Run, Integrator)
 
-from .pressure_controller import PressureController
-from .temperature_controller import TemperatureController
-from .non_bonded_controller import (NonBondedController)
+from .pressure_controller import (PressureController, PressureCouplingType, IsotropyType,
+                                  ReferenceCoordinateScalingType)
+
+from .temperature_controller import (TemperatureController, TemperatureCouplingType)
+
+from .non_bonded_controller import (NonBondedController,
+                                    CoulombType, CoulombModifierType,
+                                    VanDerWaalsType, VanDerWaalsModifierType)
+
 from .constraint_controller import (ConstraintController, ConstraintAlgorithmType, ConstraintType)
