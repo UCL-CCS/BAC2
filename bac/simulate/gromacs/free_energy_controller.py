a47ebdedaf3eee0403f91fd442d22e72cd16386d
kristofarkas <kristof.farkas-pall.14@ucl.ac.uk>
2017-07-24 18:40:28 +0100

    Minor fixes

--- bac/simulate/gromacs/free_energy_controller.py
+++ bac/simulate/gromacs/free_energy_controller.py
@@ -4,7 +4,6 @@ from bac.utils.decorators import decimal
 class FreeEnergyController:
     def __init__(self, **kwargs):
         self.expanded = kwargs.get('expanded')
-        self.initial_lambda = kwargs.get('')
-        self.delta_lambdas = kwargs.get('')
-        self.initial_lambda_state = kwargs.get('')
-        sek
+        self.initial_lambda = kwargs.get('initial_lambda')
+        self.delta_lambdas = kwargs.get('delta_lambdas')
+        self.initial_lambda_state = kwargs.get('initial_lambda_state')
--- bac/simulate/namd/__init__.py
+++ bac/simulate/namd/__init__.py
@@ -1,3 +1,4 @@
 from .run_controller import Run
 from .temperature_controller import (TemperatureController, LangevinDynamics, TemperatureCoupling,
                                      VelocityReassignment, VelocityRescaling, LoweAndersenDynamics)
+from .free_energy_controller import (FreeEnergyController, FreeEnergyCalculationType)
