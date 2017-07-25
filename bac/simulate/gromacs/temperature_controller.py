db6cee56a5c5ad77418806c5bcec41111d47c044
kristofarkas <kristof.farkas-pall.14@ucl.ac.uk>
2017-07-25 15:21:32 +0100

    Fix circular `Intergrator` enum import
    This is not the correct way to handle circular import. I am not sure what is though.  So `run_controller` imports `temperature_controller` which imports `run_controller.Integrator`.

--- bac/simulate/gromacs/temperature_controller.py
+++ bac/simulate/gromacs/temperature_controller.py
@@ -1,8 +1,20 @@
 from enum import Enum
 
 from bac.utils.decorators import advanced_property, integer, decimal
-from bac.simulate.gromacs.run_controller import Integrator
 
+
+class Integrator(Enum):
+    md = 'md'
+    md_vv = 'md-vv'
+    md_vv_avek = 'md-vv-avek'
+    sd = 'sd'
+    bd = 'bd'
+    steep = 'steep'
+    cg = 'cg'
+    l_bfgs = 'l-bfgs'
+    nm = 'nm'
+    tpi = 'tpi'
+    tpic = 'tpic'
 
 class TemperatureCouplingType(Enum):
     no = 'no'
