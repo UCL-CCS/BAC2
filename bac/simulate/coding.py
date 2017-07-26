04b6be37178cbb592c0acf40cb785a028ed679ed
kristofarkas <kristof.farkas-pall.14@ucl.ac.uk>
2017-07-26 12:55:43 +0100

    Add comments and aligned output

--- bac/simulate/coding.py
+++ bac/simulate/coding.py
@@ -9,39 +9,61 @@ class Engine(Enum):
 
 class Encoder:
 
+    # These are simple lambda functions that turn
+    # python types into specification correct
+    # format. A notable example is the yes/no and
+    # on/off bool problem.
     _encoders = {'yes_no': lambda x: ('no', 'yes')[x],
                  'on_off': lambda x: ('off', 'on')[x],
-                 'enum': lambda x: x.name}
+                 'enum': lambda x: x.value,
+                 'tuple': lambda x: ' '.join(str(xs) for xs in x)}
+
+    _column_size = 25
 
     @classmethod
     def encode(cls, run, engine):
 
         def serialize(obj, attributes):
-            s = ""
+            serial = ''
             for external_name, internals in attributes.items():
+                # As a simplification, if the internal and external name are the same
+                # one can omit the internal name in the YAML file. This results in
+                # `internals` to be `None`. We can set `internals` to be `external_name`.
+                if internals is None:
+                    internals = external_name
+
+                # Simple key value pair
                 if isinstance(internals, str):
                     value = obj.__getattribute__(internals)
                     if value is not None:
-                        s += "{} {}\n".format(external_name, value)
+                        serial += "{}{}{}\n".format(external_name,' '*(cls._column_size-len(external_name)), value)
+
+                # The value is a list containing the name and encoding mechanism
+                # The list has to be [name, encoding] format. Nothing else works.
                 elif isinstance(internals, list):
                     value = obj.__getattribute__(internals[0])
                     if value is not None:
                         encoder = cls._encoders[internals[1]]
-                        s += "{} {}\n".format(external_name, encoder(value))
+                        serial += "{}{}{}\n".format(external_name, ' '*(cls._column_size-len(external_name)), encoder(value))
+
+                # The value is a dictionary. Meaning that we have to go one level
+                # down in the hierarchy. This function is then called recursively on
+                # the object.
                 elif isinstance(internals, dict):
                     sub_obj = obj.__getattribute__(external_name)
 
                     name_token = internals.pop('name_token', None)
                     if name_token is not None:
                         encoder = cls._encoders[name_token[1]]
-                        s += "{} {}\n".format(name_token[0], encoder(sub_obj is not None))
+                        serial += "{}{}{}\n".format(name_token[0], ' '*(cls._column_size-len(name_token[0])), encoder(sub_obj is not None))
 
                     if sub_obj is not None:
-                        s += serialize(sub_obj, internals)
-            return s
+                        serial += serialize(sub_obj, internals)
+            return serial+'\n'
 
         return serialize(run, cls._schema(engine))
 
+    # FIXME: setuptools
     @staticmethod
     def _schema(engine):
         if engine is Engine.namd:
