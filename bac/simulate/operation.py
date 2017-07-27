356a1d23edef774b36dd277ba3bc05fd35b92fc7
kristofarkas <kristof.farkas-pall.14@ucl.ac.uk>
2017-07-27 14:56:34 +0100

    Update schema
    Change `tuple` to `iter`

--- bac/simulate/operation.py
+++ bac/simulate/operation.py
@@ -1,11 +1,35 @@
 from copy import deepcopy
+from enum import Enum
 
+
+class OperationState(Enum):
+    prep = 0
+    ready = 1
+    executing = 2
+    finished = 3
+    cancelled = 4
 
 class Operation:
 
     def __init__(self, run):
         self.run = deepcopy(run)
-        self.dependencies = []
+        self.dependencies: [Operation] = []
+        self.state = OperationState.prep
 
     def add_dependency(self, other):
         self.dependencies.append(other)
+
+    @property
+    def state(self):
+        return self._state
+
+    @state.setter
+    def state(self, new_state):
+        self._state = new_state
+
+    @property
+    def is_ready(self):
+        for dependency in self.dependencies:
+            if not (dependency.state is OperationState.finished):
+                return False
+        return True
--- bac/simulate/operation_queue.py
+++ bac/simulate/operation_queue.py
@@ -1,3 +1,5 @@
+import subprocess
+
 from .operation import Operation
 
 
@@ -15,7 +17,16 @@ class OperationQueue:
         self.add_operation(op)
 
     def execute(self):
+
+        while len(self.operations) > 0:
+            op = next(op for op in self.operations if op.is_ready)
+
+            subprocess.run([op.executable_path, ])
+
         print('Executing on {}'.format(self.resource))
+
+
+
 
 
 
