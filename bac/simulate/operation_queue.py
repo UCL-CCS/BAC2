2269f4c562b91f3146d9c20ddefc723cb00ccb8c
kristofarkas <kristof.farkas-pall.14@ucl.ac.uk>
2017-07-28 17:12:16 +0100

    Add `RunModifier` class
    Also update `OperationQueue` to use `RunModifier`

--- bac/simulate/operation_queue.py
+++ bac/simulate/operation_queue.py
@@ -1,13 +1,19 @@
 import subprocess
+import uuid
+import itertools
+from pathlib import Path
 
 from .operation import Operation
 
 
 class OperationQueue:
 
-    def __init__(self, resource):
+    def __init__(self, resource, name):
         self.resource = resource
+        self.name = name or str(uuid.uuid4())
+        self.path = Path(self.name)
         self.operations = []
+        self.modifiers = None
 
     def add_operation(self, operation):
         self.operations.append(operation)
@@ -24,6 +30,12 @@ class OperationQueue:
             subprocess.run(op.execute_path)
 
         print('Executing on {}'.format(self.resource))
+
+    def create_file_structure(self):
+        tree = [['{}_{}'.format(mod.name, s) for s in mod] for mod in self.modifiers]
+
+        for branch in itertools.product(*tree):
+            self.path.joinpath(*branch).mkdir(parents=True)
 
 
 
