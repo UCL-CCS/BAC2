b2a96bc13bfda31475d2f007a9005e8f640d5882
kristofarkas <kristof.farkas-pall.14@ucl.ac.uk>
2017-08-03 08:44:35 +0100

    Add `lambda` and `replica` modifiers
    I realise this is not a good name...

--- bac/simulate/run_modifier.py
+++ bac/simulate/run_modifier.py
@@ -1,9 +1,8 @@
-class RunModifier:
+class ReplicaModifier:
 
-    def __init__(self, name, modifier_function, count=5):
-        self.name = name
+    def __init__(self, count, name='replica'):
         self.index = count
-        self.mod = modifier_function
+        self.name = name
 
     def __iter__(self):
         return self
@@ -11,8 +10,30 @@ class RunModifier:
     def __next__(self):
         if self.index == 0:
             raise StopIteration
-        self.index = self.index-1
-        return self.index
+        self.index = self.index - 1
+        return self.do_nothing, '{}_{}'.format(self.name, self.index)
 
-    def modifier(self):
-        return self.modifier_function(self.index)
+    @staticmethod
+    def do_nothing(run):
+        pass
+
+
+class LambdaModifier:
+    def __init__(self, lambda_window_count, name='lambda'):
+        self.index = lambda_window_count
+        self.name = name
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index == 0:
+            raise StopIteration
+        self.index = self.index - 1
+        return self.lambda_window(self.index), '{}_{}'.format(self.name, self.index)
+
+    @staticmethod
+    def lambda_window(index):
+        def func(run):
+            run.free_energy_controller.initial_lambda_state = index
+        return func
