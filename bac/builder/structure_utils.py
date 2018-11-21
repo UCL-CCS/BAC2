2d72e02fcbd2f2b468d9ec6f1881190a41ff1486
David W Wright <dave.william.wright@gmail.com>
2018-11-21 09:46:55 +0000

    First attempt at updated structure utility function skeletons.

--- bac/builder/structure_utils.py
+++ bac/builder/structure_utils.py
@@ -1,11 +1,27 @@
 import parmed as pmd
+from bac.builder.utils.sequence import convert_resname_list
 
 
-def get_chain(structure):
+def chain_sequence(struct, chain_id, seq_format='letter'):
+
+    chain = struct[chain_id,:,:]
+
+    sequence = [x.name for x in chain.residues]
+
+    if seq_format == 'resname':
+        return sequence
+
+    else:
+
+        return convert_resname_list(sequence, seq_format=seq_format)
+
+
+def check_residue_type(struct, chain_id, resid, insertion):
 
     pass
 
-def number_chains(structure, use_segids=False):
+
+def get_chain_gaps(struct, chain_id):
 
     pass
 
