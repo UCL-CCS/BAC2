9e11648567da10df95e736d4c4468e92554e5456
David W Wright <dave.william.wright@gmail.com>
2018-11-22 14:44:32 +0000

    Move reading methods to HeaderInfo object

--- bac/builder/utils/header.py
+++ bac/builder/utils/header.py
@@ -6,6 +6,13 @@ from .sequence import convert_resname_list
 
 class HeaderInfo(object):
     """
+    Holds information on sequence and structure derived from PDB header.
+
+    Parameters
+    ----------
+    pdb_filename : :obj:`str`, optional
+        Path to PDB from which to read header
+
 
     Attributes
     ----------
@@ -19,12 +26,15 @@ class HeaderInfo(object):
         Disulphide bond descriptions.
     """
 
-    def __init__(self):
+    def __init__(self, pdb_filename=None):
 
         self.sequences = {}
         self.unit_transforms = {}
         self.symmetry = {}
         self.disulphides = {}
+
+        if pdb_filename is not None:
+            self.read_pdb_header(pdb_filename)
 
     def chain_sequence(self, chain_id, seq_format='letter'):
         """
@@ -55,7 +65,7 @@ class HeaderInfo(object):
 
         return convert_resname_list(sequence, seq_format=seq_format)
 
-    def add_seq_res_line(self, line):
+    def _add_seq_res_line(self, line):
         """
         Parses the input SEQRES line to obtain a list of three letter residue
         codes which are added to the `self.sequences` dictionary with the key
@@ -80,150 +90,145 @@ class HeaderInfo(object):
         else:
             sequences[chain_id] = residue_names
 
+    def read_pdb_header(self, pdb_filename):
+        """
+        Read a PDB file in order to obtain header information.
 
-def _parse_biomt(lines):
-    """ Parse BIOMT lines and return an ordered dictionary of `BioUnits`
+        Parameters
+        ----------
+        pdb_filename: str
+            Path to PDB file from which to read header
 
-    Parameters
-    ----------
-    lines : list
-        REMARK 350 lines from a PDB.
+        Returns
+        -------
+        :class:`HeaderInfo <bac.builder.utils.header.HeaderInfo>`
+            Information on PDB structure obtained from its header.
+        """
 
-    Returns
-    -------
-    biological_units : dict
-        Dictionary containing BioUnits derived from PDB header lines (keys are
-        identifiers given in the source).
-    """
+        biomt_lines = []
+        symmetry_lines = []
 
-    biological_units = OrderedDict()
+        with open(pdb_filename, 'r') as pdb:
 
-    biomol_no = None
+            for line in pdb:
 
-    for line in lines:
+                if line[:6] in ['ATOM  ', 'HETATM']:
+                    break
 
-        if 'BIOMOLECULE' in line:
-            # Start of a new biological unit
-            # Record any previous & reset variables to be read
-            # Example of line format:
-            # REMARK 350 BIOMOLECULE: 1
+                elif line.startswith('REMARK 350'):
+                    biomt_lines.append(line)
 
-            if biomol_no is not None:
+                elif line.startswith('REMARK 290   SMTRY'):
+                    symmetry_lines.append(line)
 
-                biological_units[biomol_no] = BioUnit(chains=chains,
-                                                      source=origin,
-                                                      nmer=nmer,
-                                                      transforms=matrices)
+                elif line.startswith('SEQRES'):
+                    self._add_seq_res_line(line)
 
-            matrices = []
-            current_matrix = []
-            nmer = ''
-            chains=[]
+        self.unit_transforms = self._parse_biomt(biomt_lines)
+        self.symmetry = self._parse_symmetry(symmetry_lines)
 
-            biomol_no = int(line.split()[3])
+    @staticmethod
+    def _parse_biomt(lines):
+        """ Parse BIOMT lines and return an ordered dictionary of `BioUnits`
 
-        elif 'DETERMINED' in line:
-            # Is this an author or software determined unit?
-            # What order of multimer is indicated?
-            # Example of line format:
-            # REMARK 350 SOFTWARE DETERMINED QUATERNARY STRUCTURE: DIMERIC
-            nmer = line.split(':')[1]
-            origin = line.split()[2]
+        Parameters
+        ----------
+        lines : list
+            REMARK 350 lines from a PDB.
 
-        elif 'APPLY THE FOLLOWING' in line:
-            # Which chains does this apply to?
-            # Example of line format:
-            # REMARK 350 APPLY THE FOLLOWING TO CHAINS: A,B
+        Returns
+        -------
+        biological_units : dict
+            Dictionary containing BioUnits derived from PDB header lines (keys are
+            identifiers given in the source).
+        """
 
-            chain_txt = line.split(':')[1]
-            chains = [x.strip() for x in chain_txt.split(',')]
+        biological_units = OrderedDict()
 
-        elif '350   BIOMT' in line:
-            # Parse the transformation matrices, provided for each
-            # additional copy of the system elements to be created
-            # Matrix is aplit across three lines (BIOMT1, BIOMT2, BIOMT3)
-            # Example of line format:
-            # REMARK 350   BIOMT1   1  1.000000  0.000000  0.000000        0.00000
+        biomol_no = None
 
-            columns = line.split()
-            row_label = columns[2][-1]
-            current_matrix.append(columns[4:8])
+        for line in lines:
 
-            # BioUnit requires a 3*4 matrix containing translation and
-            # rotation information
-            if row_label == '3':
-                matrices.append(BioTransform(current_matrix))
+            if 'BIOMOLECULE' in line:
+                # Start of a new biological unit
+                # Record any previous & reset variables to be read
+                # Example of line format:
+                # REMARK 350 BIOMOLECULE: 1
+
+                if biomol_no is not None:
+
+                    biological_units[biomol_no] = BioUnit(chains=chains,
+                                                          source=origin,
+                                                          nmer=nmer,
+                                                          transforms=matrices)
+
+                matrices = []
                 current_matrix = []
+                nmer = ''
+                chains=[]
 
-    biological_units[biomol_no] = BioUnit(chains=chains, transforms=matrices,
-                                          source=origin, nmer=nmer)
+                biomol_no = int(line.split()[3])
 
-    return biological_units
+            elif 'DETERMINED' in line:
+                # Is this an author or software determined unit?
+                # What order of multimer is indicated?
+                # Example of line format:
+                # REMARK 350 SOFTWARE DETERMINED QUATERNARY STRUCTURE: DIMERIC
+                nmer = line.split(':')[1]
+                origin = line.split()[2]
 
+            elif 'APPLY THE FOLLOWING' in line:
+                # Which chains does this apply to?
+                # Example of line format:
+                # REMARK 350 APPLY THE FOLLOWING TO CHAINS: A,B
 
-def _parse_symmetry(lines):
-    """
-    Parse symmetry information from REMARK 290 PDB header lines.
+                chain_txt = line.split(':')[1]
+                chains = [x.strip() for x in chain_txt.split(',')]
 
-    Parameters
-    ----------
-    lines : str
-        REMARK 290 lines from a PDB.
+            elif '350   BIOMT' in line:
+                # Parse the transformation matrices, provided for each
+                # additional copy of the system elements to be created
+                # Matrix is aplit across three lines (BIOMT1, BIOMT2, BIOMT3)
+                # Example of line format:
+                # REMARK 350   BIOMT1   1  1.000000  0.000000  0.000000        0.00000
 
-    Returns
-    -------
-    :class:`Symmetry <bac.builder.utils.assembly.Symmetry>`
-        Tensor describing crystal symmetry
-    """
+                columns = line.split()
+                row_label = columns[2][-1]
+                current_matrix.append(columns[4:8])
 
-    data = []
+                # BioUnit requires a 3*4 matrix containing translation and
+                # rotation information
+                if row_label == '3':
+                    matrices.append(BioTransform(current_matrix))
+                    current_matrix = []
 
-    for line in lines:
-        if line.strip().startswith('REMARK 290   SMTRY'):
-            data.append(line.split()[4:])
+        biological_units[biomol_no] = BioUnit(chains=chains, transforms=matrices,
+                                              source=origin, nmer=nmer)
 
-    tensor = np.asarray(data, dtype='f8')
+        return biological_units
 
-    return Symmetry(tensor)
+    @staticmethod
+    def _parse_symmetry(lines):
+        """
+        Parse symmetry information from REMARK 290 PDB header lines.
 
+        Parameters
+        ----------
+        lines : str
+            REMARK 290 lines from a PDB.
 
-def read_pdb_header(pdb_filename):
-    """
-    Read a PDB file in order to obtain header information.
+        Returns
+        -------
+        :class:`Symmetry <bac.builder.utils.assembly.Symmetry>`
+            Tensor describing crystal symmetry
+        """
 
-    Parameters
-    ----------
-    pdb_filename: str
-        Path to PDB file from which to read header
+        data = []
 
-    Returns
-    -------
-    :class:`HeaderInfo <bac.builder.utils.header.HeaderInfo>`
-        Information on PDB structure obtained from its header.
-    """
+        for line in lines:
+            if line.strip().startswith('REMARK 290   SMTRY'):
+                data.append(line.split()[4:])
 
-    header_info = HeaderInfo()
+        tensor = np.asarray(data, dtype='f8')
 
-    biomt_lines = []
-    symmetry_lines = []
-
-    with open(pdb_filename, 'r') as pdb:
-
-        for line in pdb:
-
-            if line[:6] in ['ATOM  ', 'HETATM']:
-                break
-
-            elif line.startswith('REMARK 350'):
-                biomt_lines.append(line)
-
-            elif line.startswith('REMARK 290   SMTRY'):
-                symmetry_lines.append(line)
-
-            elif line.startswith('SEQRES'):
-                header_info.add_seq_res_line(line)
-
-    header_info.unit_transforms = _parse_biomt(biomt_lines)
-    header_info.symmetry = _parse_symmetry(symmetry_lines)
-
-    return header_info
+        return Symmetry(tensor)
