95e10d0ac3de3029b090cae47ffa5496eee40cae
David W Wright <dave.william.wright@gmail.com>
2018-11-23 10:29:30 +0000

    Tidying

--- bac/builder/source_structure.py
+++ bac/builder/source_structure.py
@@ -10,7 +10,7 @@ class SourceStructure(object):
     def __init__(self, structure_filename):
 
         if os.path.splitext(structure_filename)[1] != '.pdb':
-            raise NotImplementedError("We can only process PDB files at present")
+            raise NotImplementedError("We can only process PDB files for now.")
 
         self.structure = pmd.load_file(structure_filename)
         self.header = HeaderInfo(structure_filename)
@@ -26,7 +26,7 @@ class SourceStructure(object):
             chain_types[chain] = scan_chain_type(self, chain)
 
             # Record what is nonstandard before we update types to consider
-            # bonding pattern for residues linked to polymers (protein, RNA, DNA).
+            # bonding for residues linked to polymers (protein, RNA, DNA).
             condition = (chain_types[chain][:, 1] == 'unknown')
             self.chain_nonstandard[chain] = chain_types[chain][condition][:, 0]
 
