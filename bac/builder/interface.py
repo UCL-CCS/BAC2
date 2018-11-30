7f422705b995148bd0da3333ba0efc5f8cfcbcd8
David W Wright <dave.william.wright@gmail.com>
2018-11-30 13:53:27 +0000

    Template function for model building interface.

---
+++ bac/builder/interface.py
@@ -0,0 +1,8 @@
+
+
+def prepare_model(mols=list(), leaprcs=list(), ff_choice='ff14sb', ff_exts=list(),
+                  custom_atoms=list(), water_model='TIP3',
+                  box_size=10, salt_con=0,
+                  salt_anions='Cl-', salt_cations='Na+',
+                  disulph=list(), membrane=False, chain_caps=dict()):
+    pass
