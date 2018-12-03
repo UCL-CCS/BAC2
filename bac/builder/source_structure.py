25b5d517e857b9d4c55f6168bf5e93d81b6a56c5
David W Wright <dave.william.wright@gmail.com>
2018-12-03 14:58:34 +0000

    Working code to get a sequence with gaps from coordinates in PDB

--- bac/builder/source_structure.py
+++ bac/builder/source_structure.py
@@ -4,6 +4,7 @@ import parmed as pmd
 from bac.builder.utils.header import HeaderInfo
 from bac.builder.structure_utils import (scan_chain_type, update_chain_type_assignment,
                                          get_polymer_gaps)
+from bac.builder.utils.sequence import convert_resname_list
 
 
 class SourceStructure(object):
@@ -134,7 +135,118 @@ class SourceStructure(object):
         residue_numbers = [(residue.idx, residue.number) for residue
                            in residues if residue.idx in idxs]
 
+        residue_numbers = np.array(residue_numbers)
+
         return [arr[:, 0] for arr in
                 np.split(residue_numbers,
                          np.where(np.diff(residue_numbers[:, 1]) < 0)[0] + 1)
                 if arr.size > 1]
+
+    def residue_contiguous_no_blocks(self, idxs):
+        """
+        Get list of blocks of residue indexes where the residue numbers
+        are contiguous.
+
+        Parameters
+        ----------
+        idxs : list or np.array
+            Indices of the residues to group
+
+        Returns
+        -------
+        list
+            List of `np.arrays` containing indices of residues which increase in
+            residue numbering.
+        """
+
+        residues = self.structure.residues
+
+        residue_numbers = [(residue.idx, residue.number) for residue
+                           in residues if residue.idx in idxs]
+
+        residue_numbers = np.array(residue_numbers)
+
+        return [arr[:, 0] for arr in
+                np.split(residue_numbers,
+                         np.where(np.diff(residue_numbers[:, 1]) != 0)[0] + 1)
+                if arr.size > 1]
+
+    def chain_sequence(self, chain_id, seq_format='letter', gap_char='-'):
+        """
+        Get sequence information for selected chain in input structure.
+
+        Parameters
+        ----------
+        chain_id : str
+            Selected chain identifier.
+        seq_format : str
+            Format for output - 'resname' (list of three letter reside codes),
+            'letter' (list of single letter residue codes), 'fasta' (sting of
+            single letter codes).
+        gap_char : str
+            Character to use in single letter format for gaps.
+
+        Returns
+        -------
+        list or str
+            Output of all residue codes in the format selected in seq_format.
+        """
+
+        if len(gap_char) > 1:
+            raise RuntimeError("Must use a single character to represent "
+                               "sequence gaps")
+
+        struct = self.structure
+
+        residues = self.structure.residues
+        residue_types = self.chain_types[chain_id]
+
+        if chain_id in self.chain_gaps:
+            gaps = self.chain_gaps[chain_id]
+        else:
+            gaps = []
+
+        if not np.any(np.isin(residue_types[:, 1], ['protein', 'dna', 'rna'])):
+            if seq_format == 'fasta':
+                return ''
+            else:
+                return []
+
+        mask = np.isin(residue_types[:, 1], ['protein', 'dna', 'rna'])
+        seq_idxs = residue_types[:, 0][mask]
+
+        residue_blocks = self.residue_contiguous_no_blocks(seq_idxs)
+
+        gap_fill = {}
+
+        for gap in gaps:
+
+            start = residues[gap[0]].number
+            end = residues[gap[1]].number
+
+            gap_fill[start] = [gap_char for _ in range(end-start)]
+
+        if seq_format == 'fasta':
+            sequence = ''
+        else:
+            sequence = []
+
+        for residue_block in residue_blocks:
+
+            block_sequence = [residues[x].name for x in residue_block]
+
+            if seq_format != 'resname':
+                block_sequence = convert_resname_list(block_sequence,
+                                                      seq_format=seq_format)
+
+            last_residue = residues[residue_block[-1]].number
+
+            sequence += block_sequence
+
+            if last_residue in gap_fill:
+                if seq_format == 'fasta':
+                    sequence += ''.join(gap_fill[last_residue])
+                else:
+                    sequence += gap_fill[last_residue]
+
+        return sequence
