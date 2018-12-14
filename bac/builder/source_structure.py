3bd4f07040521670f4c51c0a10a00487b117856a
David W Wright <dave.william.wright@gmail.com>
2018-12-14 11:53:54 +0000

    Add docstrings and make minor (mostly aesthetic) edits inspired by pylint run on the file

--- bac/builder/source_structure.py
+++ bac/builder/source_structure.py
@@ -1,3 +1,18 @@
+"""
+The aim of the classes here is to provide a layer on top of ParmEd to help read
+in structure files (currently on PDB files) and detect modellable elements -
+i.e. protein, nucleic acid chains and ligands.
+
+Classes
+-------
+
+Subdivision: Stored information on single type components (e.g. protein chain).
+             Also provides basic functionality to align with a FASTA sequence
+             where appropriate for modelling.
+
+SourceStructure: Stores information on PDB structure and header and provides
+                 functions to decompose into `Subdivisions`.
+"""
 import os
 from collections import defaultdict
 import pprint
@@ -11,7 +26,23 @@ from bac.builder.structure_utils import (get_residue_type,
                                          residue_list_sequence)
 
 
-class Subdivision(object):
+class Subdivision:
+    """
+    Stores information on single type (i.e. protein, dna, rna, water ...)
+    subset of residues in a structure (from a single PDB chain) and provides
+    functions to perform basic alignment to FASTA sequences (where
+    appropriate) and derive information for structural modelling.
+
+    Parameters
+    ----------
+    residue_type: str
+        What type of residues does the subdivision refer to - protein, dna,
+        rna, water, ion or unknown.
+    chain_id : str
+        Which PDB chain is the subdivision derived from.
+    structure: :class:`Structure <parmed.Structure>`
+        Structure from which the subdivision is to be derived.
+    """
 
     def __init__(self, residue_type=None, chain_id='', structure=None):
 
@@ -25,6 +56,16 @@ class Subdivision(object):
         self.src_structure = structure
 
     def is_polymer(self):
+        """
+        Check if the `self.residue_type` indicates that the `Subdivision`
+        represents a known polymer type - i.e. is one of 'protein, 'dna' or
+        'rna'.
+
+        Returns
+        -------
+        bool:
+            Is this a protein or nucleic acid polymer?
+        """
 
         if self.residue_type in ['protein', 'dna', 'rna']:
             return True
@@ -32,11 +73,52 @@ class Subdivision(object):
         return False
 
     def sequence(self, gap_char='-', seq_format='fasta'):
+        """
+        Return the sequence of the `self.residues` list of residues, with gaps
+        between residues listed using `gap_char`. The output can be lists of
+        one or three character residue codes or a FASTA format string (depending
+        on the `seq_format` chosen).
+
+        Parameters
+        ----------
+        gap_char : str
+            Character to use when representing gaps in polymers (will be
+            replicated three times in three letter code lists).
+        seq_format : str
+            Specifies the returned sequence format: 'letter' = single letter
+            code list, 'rescode' = three letter code list and 'fasta' = FASTA
+            format string.
+
+        Returns
+        -------
+        list or str
+            Either a list of residue codes (one or three letter) or a FASTA
+            format string, depending on the `seq_format` chosen.
+
+        """
 
         return residue_list_sequence(self.residues, seq_format=seq_format,
                                      gap_char=gap_char)
 
     def align_sequence(self, sequence):
+        """
+        Attempt to aligned `sequence` with `self.residues` using a basic string
+        matching. If possible `self.aligned_sequence` is set to the subsection
+        of `sequence` which overlaps the structural residues,
+        `self.pre_sequence` any parts of sequence that come before the overlap
+        and `self.pre_sequence` any that come after it.
+
+        Parameters
+        ----------
+        sequence: str
+            FASTA formatted string of the target sequence.
+
+        Returns
+        -------
+        bool:
+            Could the input sequence be aligned (using very simple string
+            matching) with `self.residues` list of residues.
+        """
 
         if not self.is_polymer():
             return False
@@ -53,6 +135,31 @@ class Subdivision(object):
 
     def get_added_residue_positions(self, pre_res=0, post_res=0,
                                     numbering='modeller'):
+        """
+        Get the locations where residues would be added if a model was created
+        to fill in gaps detected in the `self.residues` list and extra residues
+        of the specified number were added pre and post structure.
+
+        Parameters
+        ----------
+        pre_res : int
+            Number of residues intended to be added before the structural
+            residues.
+        post_res : int
+            Number of residues intended to be added after the structural
+            residues.
+        numbering : str
+            Numbering scheme to use - default is to start at 1 (specified as
+            'modeller' as that is what is used by the Modeller software)
+            otherwise just use the residue number.
+
+        Returns
+        -------
+        list:
+            List of tuples containing first and last residue number given the
+            specified numbering scheme.
+
+        """
 
         if numbering == 'modeller':
             offset = 0
@@ -65,6 +172,26 @@ class Subdivision(object):
                 in re.finditer('-+', sequence)]
 
     def target_sequence(self, pre_res=0, post_res=0):
+        """
+        Get FASTA format sequence form modelling - include specified regions
+        before and after overlapping region.
+
+        Parameters
+        ----------
+        pre_res : int
+            Number of residues intended to be added before the structural
+            residues.
+        post_res : int
+            Number of residues intended to be added after the structural
+            residues.
+
+        Returns
+        -------
+        str:
+            FASTA style sequence of aligned sequence and selected length
+            additions before and after the structure in `self.residues`
+
+        """
 
         aligned_sequence = self.aligned_sequence
 
@@ -77,15 +204,41 @@ class Subdivision(object):
             return ''
 
     def structure(self):
+        """
+        Copy the residues contained in the subdivision into a new
+        `parmed.Structure`.
+
+        Returns
+        -------
+        structure: :class:`Structure <parmed.Structure>`
+            A `Structure` from `self.src_structure` which contains only
+            the residues in this subversion.
+        """
 
         struct = self.src_structure
 
-        df = struct.to_dataframe()
+        dataframe = struct.to_dataframe()
         residue_idxs = [res.idx for res in self.residues]
 
-        return struct[df.resid.isin(residue_idxs)]
+        return struct[dataframe.resid.isin(residue_idxs)]
 
     def modeller_selection(self, filename):
+        """
+        Create a modeller selection string for use in a PIR format alignment
+        file to load the correct residues from a PDB created from the
+        subdivision.
+
+        Parameters
+        ----------
+        filename: str
+            Name of the PDB file from which the coordinates will be read.
+
+        Returns
+        -------
+        str:
+            'structure' line for use in Modeller PIR alignment files.
+
+        """
 
         if filename[-4:] == '.pdb':
             pdbname = filename[:-4]
@@ -96,7 +249,7 @@ class Subdivision(object):
         start = self.residues[0].resid
         end = self.residues[-1].resid
 
-        return (f"structure:{filename}:{start}:{chain_id}:{end}:{chain_id}"
+        return (f"structure:{pdbname}:{start}:{chain_id}:{end}:{chain_id}"
                 f"::::")
 
     def __repr__(self):
@@ -118,15 +271,32 @@ class Subdivision(object):
             first_idx = None
             last_idx = None
 
-        return (f"{self.__class__}( residue_idxs={first_idx}-{last_idx}," 
-                f" residue_type={residue_type}, aligned_sequence={aligned_sequence})")
+        return (f"{self.__class__}( residue_idxs={first_idx}-{last_idx}, "
+                f"residue_type={residue_type}, "
+                f"aligned_sequence={aligned_sequence})")
 
     def __str__(self):
 
         return pprint.pformat(self.__dict__, indent=4)
 
 
-class SourceStructure(object):
+class SourceStructure:
+    """
+    Stores a linked `parmed.structure` with header information and methods that
+    produce a decomposition into single type subdivions + other functions
+    designed to help model building.
+
+    Parameters
+    ----------
+    structure: :class:`Structure <parmed.Structure>` or str
+        Either a ParmEd structure or path to a structure file (only PDBs
+        supported at present).
+    header_filename :
+        File from which to read a header (intended for when a structure is
+        passed, if `structure` is a filename the header is read from that
+        by default).
+
+    """
 
     def __init__(self, structure, header_filename=None):
 
@@ -154,19 +324,19 @@ class SourceStructure(object):
         self.chain_map = defaultdict(pmd.TrackedList)
         self.nonstandard = []
 
-        for r in struct.residues:
+        for residue in struct.residues:
 
-            residue_type = get_residue_type(r)
+            residue_type = get_residue_type(residue)
 
             if residue_type == 'unknown':
-                self.nonstandard.append(r)
-                polymer_bond = check_residue_polymer_bonded(r)
+                self.nonstandard.append(residue)
+                polymer_bond = check_residue_polymer_bonded(residue)
                 if polymer_bond is not None:
                     residue_type = polymer_bond
 
             # TODO: Is there a less hacky way to do this?
-            r.residue_type = residue_type
-            self.chain_map[r.chain].append(r)
+            residue.residue_type = residue_type
+            self.chain_map[residue.chain].append(residue)
 
         self.chains = self.chain_map.keys()
         self.decomposition = self.default_decomposition()
@@ -178,6 +348,19 @@ class SourceStructure(object):
                 f"header_file={self.header_filename})")
 
     def default_decomposition(self):
+        """
+        Create a dictionary of lists of `Subdivisions` describing contiguous
+        regions of each chain which have the same residue type (protein, rna,
+        dna, water, ion, unknown).
+
+        Returns
+        -------
+        dict:
+            keys = PDB chain ids and values = list of `Subdivisions`. Each
+            `Subdivision` refers to a contiguous region of residues of the same
+            type.
+
+        """
 
         structure = self.structure
         chain_map = self.chain_map
@@ -220,8 +403,8 @@ class SourceStructure(object):
                         if not bonded:
                             subdivision.gaps.append((last_residue, residue))
 
-            if residue == chain_residues[-1]:
-                decomposition[chain].append(subdivision)
+                if residue == chain_residues[-1]:
+                    decomposition[chain].append(subdivision)
 
         return decomposition
 
@@ -236,11 +419,27 @@ class SourceStructure(object):
 
         pass
 
-    def create_scaffold(self, selection, naming_scheme):
-
-        pass
-
     def chain_sequence(self, chain_id, seq_format='fasta', src='structure'):
+        """
+        Return a representation of the residues contained in the selected PDB
+        chain. The output can be lists of one or three character residue codes
+        or a FASTA format string (depending on the `seq_format` chosen). All
+        residues in the chain no matter what type are included.
+
+        Parameters
+        ----------
+        chain_id : str
+            PDB chain ID for which to produce sequence.
+        seq_format : str
+            Specifies the returned sequence format: 'letter' = single letter
+            code list, 'rescode' = three letter code list and 'fasta' = FASTA
+            format string.
+        src
+
+        Returns
+        -------
+
+        """
 
         if src not in ['structure', 'header']:
             raise RuntimeError("src must be 'structure', 'header'")
@@ -250,11 +449,12 @@ class SourceStructure(object):
 
         if src == 'header':
 
-            three_letter_seq = header_info.sequence
+            three_letter_seq = header_info.sequences[chain_id]
 
             if seq_format == 'fasta':
 
-                sequence = convert_resname_list(three_letter_seq, seq_format=seq_format)
+                sequence = convert_resname_list(three_letter_seq,
+                                                seq_format=seq_format)
 
             elif seq_format == 'letter':
 
