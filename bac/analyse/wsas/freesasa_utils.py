275b58815b0e77df2e05e925b3c3ec81c5f7d070
bieniekmat <bieniekmat@gmail.com>
2020-03-23 11:51:48 +0000

    Revert removal of the parameters for now.

--- bac/analyse/wsas/freesasa_utils.py
+++ bac/analyse/wsas/freesasa_utils.py
@@ -15,10 +15,10 @@ _DEFAULT_OPTIONS = {
 
 _DEFAULT_PARAMETERS = {
     'algorithm': freesasa.LeeRichards,
-    # 'probe-radius': freesasa.defaultParameters['probe-radius'],
-    # 'n-points': freesasa.defaultParameters['n-points'],
-    # 'n-slices': freesasa.defaultParameters['n-slices'],
-    # 'n-threads': freesasa.defaultParameters['n-threads']
+    'probe-radius': freesasa.defaultParameters['probe-radius'],
+    'n-points': freesasa.defaultParameters['n-points'],
+    'n-slices': freesasa.defaultParameters['n-slices'],
+    'n-threads': freesasa.defaultParameters['n-threads']
 }
 
 
